42b14bc5c5fdb15e532ac3835820f92d21d72994
Andrew Ackerman <36349296+ackers93@users.noreply.github.com>
2025-10-22 10:05:00 -0700

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const sass = require("sass");
+
+module.exports = function(eleventyConfig) {
+  // Copy static assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/css");
+
+  // Watch for CSS changes
+  eleventyConfig.addWatchTarget("./src/css/main.css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts"
+    },
+    templateFormats: ["html", "md", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+}; 
